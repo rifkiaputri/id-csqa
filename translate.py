085bf69a660f68cebad23ed37413508c9dd99e1f
Faiz Ghifari Haznitrama <fgh197@live.com>
2023-09-26 11:31:11 +0900

    add code for translate using sm4t

---
+++ translate.py
@@ -0,0 +1,74 @@
+import csv
+import ast
+import torch
+import torchaudio
+
+from glob import glob
+from tqdm import tqdm
+from seamless_communication.models.inference import Translator
+
+
+def load_csv_data(file_path):
+    data_list = []
+
+    with open(file_path, newline="") as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            row["choices"] = ast.literal_eval(row["choices"])
+            data_list.append(row)
+
+    return data_list
+
+
+def save_data(samples, file_path):
+    header = samples[0].keys()
+
+    with open(file_path, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=header)
+        writer.writeheader()
+        for row in samples:
+            writer.writerow(row)
+
+    print(f'CSV file "{file_path}" has been created with the data.')
+
+
+model = "seamlessM4T_medium"
+vocoder_model = "vocoder_36langs"
+
+translator = Translator(model, vocoder_model, torch.device("cuda:0"), torch.float16)
+
+for folder in glob("./eval/*/"):
+    for file in glob(f"{folder}*.csv"):
+        if "v3" in file and "v3_test_name" not in file:
+            fname = file.split("/")[-1]
+            print(f"Translating file {fname}")
+
+            file_data = load_csv_data(file)
+            for data in tqdm(file_data):
+                question, _, _ = translator.predict(
+                    data["question"],
+                    "t2tt",
+                    "ind",
+                    src_lang="eng",
+                    ngram_filtering=True,
+                )
+                data["question"] = str(question)
+
+                concept, _, _ = translator.predict(
+                    data["question_concept"],
+                    "t2tt",
+                    "ind",
+                    src_lang="eng",
+                    ngram_filtering=True,
+                )
+                data["question_concept"] = str(concept).lower()
+
+                for i in range(len(data["choices"]["text"])):
+                    opt = data["choices"]["text"][i]
+                    opt_text, _, _ = translator.predict(
+                        opt, "t2tt", "ind", src_lang="eng", ngram_filtering=True
+                    )
+                    data["choices"]["text"][i] = str(opt_text).lower()
+
+            translated_file = f"{folder}translated_sm4t_med_{fname}"
+            save_data(file_data, translated_file)
