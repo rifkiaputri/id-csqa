49943a58ab094b89eea431c821c02b78929466be
Rifki Afina Putri <rifkiafinaputri@gmail.com>
2023-12-14 16:21:25 +0900

    Move script

---
+++ evaluation/scripts/run_eval.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "aisingapore/sealion7b-instruct-nc" \
+    --dataset_path "../dataset/v1_adapt/ind/filtered_both_test.csv" \
+    --gold_key "answerKey" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "aisingapore/sealion7b-instruct-nc" \
+    --dataset_path "../dataset/v1_adapt/sun/filtered_both_test.csv" \
+    --gold_key "answerKey" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "aisingapore/sealion7b-instruct-nc" \
+    --dataset_path "../dataset/v2_human/filtered_ind.json" \
+    --gold_key "answer_majority" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "aisingapore/sealion7b-instruct-nc" \
+    --dataset_path "../dataset/v2_human/filtered_sun.json" \
+    --gold_key "answer_majority" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
--- evaluation/scripts/run_eval_gpt.sh
+++ evaluation/scripts/run_eval_gpt.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=4 python eval.py \
-    --model_name "aisingapore/sealion7b" \
+python eval.py \
+    --model_name "gpt-3.5-turbo" \
     --dataset_path "../dataset/v1_adapt/ind/filtered_both_test.csv" \
     --gold_key "answerKey" \
     --history_version "231205" \
     --batch_size 10 \
     --prompt_type 1
 
-CUDA_VISIBLE_DEVICES=4 python eval.py \
-    --model_name "aisingapore/sealion7b" \
+python eval.py \
+    --model_name "gpt-3.5-turbo" \
     --dataset_path "../dataset/v1_adapt/sun/filtered_both_test.csv" \
     --gold_key "answerKey" \
     --history_version "231205" \
     --batch_size 10 \
     --prompt_type 1
 
-CUDA_VISIBLE_DEVICES=4 python eval.py \
-    --model_name "aisingapore/sealion7b" \
+python eval.py \
+    --model_name "gpt-3.5-turbo" \
     --dataset_path "../dataset/v2_human/filtered_ind.json" \
     --gold_key "answer_majority" \
     --history_version "231205" \
     --batch_size 10 \
     --prompt_type 1
 
-CUDA_VISIBLE_DEVICES=4 python eval.py \
-    --model_name "aisingapore/sealion7b" \
+python eval.py \
+    --model_name "gpt-3.5-turbo" \
     --dataset_path "../dataset/v2_human/filtered_sun.json" \
     --gold_key "answer_majority" \
     --history_version "231205" \
