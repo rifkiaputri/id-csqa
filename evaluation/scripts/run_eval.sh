23bf45ddebc1b82b353c386088321e2683a4bfb0
Rifki Afina Putri <rifkiafinaputri@gmail.com>
2023-12-20 17:24:48 +0900

    Add seallm results

--- evaluation/scripts/run_eval.sh
+++ evaluation/scripts/run_eval.sh
@@ -1,6 +1,70 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=4 python eval.py \
-    --model_name "aisingapore/sealion7b-instruct-nc" \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
+    --dataset_path "../dataset/v1_adapt/ind/filtered_both_test.csv" \
+    --gold_key "answerKey" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
+    --dataset_path "../dataset/v1_adapt/sun/filtered_both_test.csv" \
+    --gold_key "answerKey" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
+    --dataset_path "../dataset/v2_human/filtered_ind.json" \
+    --gold_key "answer_majority" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
+    --dataset_path "../dataset/v2_human/filtered_sun.json" \
+    --gold_key "answer_majority" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
+    --dataset_path "../dataset/v1_adapt/ind/filtered_both_test.csv" \
+    --gold_key "answerKey" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
+    --dataset_path "../dataset/v1_adapt/sun/filtered_both_test.csv" \
+    --gold_key "answerKey" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
+    --dataset_path "../dataset/v2_human/filtered_ind.json" \
+    --gold_key "answer_majority" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
+    --dataset_path "../dataset/v2_human/filtered_sun.json" \
+    --gold_key "answer_majority" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=4 python eval.py \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
     --dataset_path "../dataset/v1_adapt/ind/filtered_both_test.csv" \
     --gold_key "answerKey" \
     --history_version "231205" \
@@ -8,7 +72,7 @@ CUDA_VISIBLE_DEVICES=4 python eval.py \
     --prompt_type 1
 
 CUDA_VISIBLE_DEVICES=4 python eval.py \
-    --model_name "aisingapore/sealion7b-instruct-nc" \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
     --dataset_path "../dataset/v1_adapt/sun/filtered_both_test.csv" \
     --gold_key "answerKey" \
     --history_version "231205" \
@@ -16,7 +80,7 @@ CUDA_VISIBLE_DEVICES=4 python eval.py \
     --prompt_type 1
 
 CUDA_VISIBLE_DEVICES=4 python eval.py \
-    --model_name "aisingapore/sealion7b-instruct-nc" \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
     --dataset_path "../dataset/v2_human/filtered_ind.json" \
     --gold_key "answer_majority" \
     --history_version "231205" \
@@ -24,7 +88,7 @@ CUDA_VISIBLE_DEVICES=4 python eval.py \
     --prompt_type 1
 
 CUDA_VISIBLE_DEVICES=4 python eval.py \
-    --model_name "aisingapore/sealion7b-instruct-nc" \
+    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
     --dataset_path "../dataset/v2_human/filtered_sun.json" \
     --gold_key "answer_majority" \
     --history_version "231205" \
