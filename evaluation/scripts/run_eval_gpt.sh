5472ad55865ea1df7956d0b6a5e10a23077b485f
Rifki Afina Putri <rifkiafinaputri@gmail.com>
2024-01-18 16:03:16 +0900

    Add raw-unique results

--- evaluation/scripts/run_eval_gpt.sh
+++ evaluation/scripts/run_eval_gpt.sh
@@ -1,162 +1,144 @@
 #!/bin/bash
-# python eval.py \
-#     --model_name "gpt-3.5-turbo" \
-#     --dataset_path "../dataset/v1_adapt/ind/filtered_both_test.json" \
-#     --gold_key "answer_creator" \
-#     --history_version "231205_few-shot" \
-#     --batch_size 10 \
-#     --prompt_type 3 \
-#     --few_shot
-
-# python eval.py \
-#     --model_name "gpt-3.5-turbo" \
-#     --dataset_path "../dataset/v1_adapt/sun/filtered_both_test.json" \
-#     --gold_key "answer_creator" \
-#     --history_version "231205_few-shot" \
-#     --batch_size 10 \
-#     --prompt_type 3 \
-#     --few_shot
-
-# python eval.py \
-#     --model_name "gpt-3.5-turbo" \
-#     --dataset_path "../dataset/v1_adapt/ind/filtered_both_test.json" \
-#     --gold_key "answer_creator" \
-#     --history_version "231205_few-shot" \
-#     --batch_size 10 \
-#     --prompt_type 2 \
-#     --few_shot
-
-# python eval.py \
-#     --model_name "gpt-3.5-turbo" \
-#     --dataset_path "../dataset/v1_adapt/sun/filtered_both_test.json" \
-#     --gold_key "answer_creator" \
-#     --history_version "231205_few-shot" \
-#     --batch_size 10 \
-#     --prompt_type 2 \
-#     --few_shot
-
-# python eval.py \
-#     --model_name "gpt-3.5-turbo" \
-#     --dataset_path "../dataset/v1_adapt/ind/filtered_both_test.json" \
-#     --gold_key "answer_creator" \
-#     --history_version "231205_few-shot" \
-#     --batch_size 10 \
-#     --prompt_type 1 \
-#     --few_shot
-
-# python eval.py \
-#     --model_name "gpt-3.5-turbo" \
-#     --dataset_path "../dataset/v1_adapt/sun/filtered_both_test.json" \
-#     --gold_key "answer_creator" \
-#     --history_version "231205_few-shot" \
-#     --batch_size 10 \
-#     --prompt_type 1 \
-#     --few_shot
-
-python eval.py \
+CUDA_VISIBLE_DEVICES=0 python eval.py \
     --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v2_human/filtered_ind.json" \
-    --gold_key "answer_majority" \
-    --history_version "231205_few-shot" \
-    --batch_size 10 \
-    --prompt_type 3 \
-    --few_shot
-
-python eval.py \
-    --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v2_human/filtered_sun.json" \
-    --gold_key "answer_majority" \
-    --history_version "231205_few-shot" \
-    --batch_size 10 \
-    --prompt_type 3 \
-    --few_shot
-
-python eval.py \
-    --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v2_human/filtered_ind.json" \
-    --gold_key "answer_majority" \
-    --history_version "231205_few-shot" \
-    --batch_size 10 \
-    --prompt_type 2 \
-    --few_shot
-
-python eval.py \
-    --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v2_human/filtered_sun.json" \
-    --gold_key "answer_majority" \
-    --history_version "231205_few-shot" \
-    --batch_size 10 \
-    --prompt_type 2 \
-    --few_shot
-
-python eval.py \
-    --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v2_human/filtered_ind.json" \
-    --gold_key "answer_majority" \
-    --history_version "231205_few-shot" \
-    --batch_size 10 \
-    --prompt_type 1 \
-    --few_shot
-
-python eval.py \
-    --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v2_human/filtered_sun.json" \
-    --gold_key "answer_majority" \
-    --history_version "231205_few-shot" \
-    --batch_size 10 \
-    --prompt_type 1 \
-    --few_shot
-
-python eval.py \
-    --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v3_synthetic/filtered_ind.json" \
+    --dataset_path "../dataset/v3_synthetic/ind/filtered_test.json" \
     --gold_key "answer_creator" \
-    --history_version "231205_few-shot" \
+    --history_version "231205" \
     --batch_size 10 \
-    --prompt_type 3 \
-    --few_shot
+    --prompt_type 1
 
-python eval.py \
+CUDA_VISIBLE_DEVICES=0 python eval.py \
     --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v3_synthetic/filtered_sun.json" \
+    --dataset_path "../dataset/v3_synthetic/sun/filtered_test.json" \
     --gold_key "answer_creator" \
-    --history_version "231205_few-shot" \
+    --history_version "231205" \
     --batch_size 10 \
-    --prompt_type 3 \
-    --few_shot
+    --prompt_type 1
 
-python eval.py \
+CUDA_VISIBLE_DEVICES=0 python eval.py \
     --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v3_synthetic/filtered_ind.json" \
+    --dataset_path "../dataset/v3_synthetic/ind/filtered_test.json" \
     --gold_key "answer_creator" \
-    --history_version "231205_few-shot" \
+    --history_version "231205" \
     --batch_size 10 \
-    --prompt_type 2 \
-    --few_shot
+    --prompt_type 2
 
-python eval.py \
+CUDA_VISIBLE_DEVICES=0 python eval.py \
     --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v3_synthetic/filtered_sun.json" \
+    --dataset_path "../dataset/v3_synthetic/sun/filtered_test.json" \
     --gold_key "answer_creator" \
-    --history_version "231205_few-shot" \
+    --history_version "231205" \
     --batch_size 10 \
-    --prompt_type 2 \
-    --few_shot
+    --prompt_type 2
 
-python eval.py \
+CUDA_VISIBLE_DEVICES=0 python eval.py \
     --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v3_synthetic/filtered_ind.json" \
+    --dataset_path "../dataset/v3_synthetic/ind/filtered_test.json" \
     --gold_key "answer_creator" \
-    --history_version "231205_few-shot" \
+    --history_version "231205" \
     --batch_size 10 \
-    --prompt_type 1 \
-    --few_shot
+    --prompt_type 3
 
-python eval.py \
+CUDA_VISIBLE_DEVICES=0 python eval.py \
     --model_name "gpt-3.5-turbo" \
-    --dataset_path "../dataset/v3_synthetic/filtered_sun.json" \
+    --dataset_path "../dataset/v3_synthetic/sun/filtered_test.json" \
     --gold_key "answer_creator" \
-    --history_version "231205_few-shot" \
+    --history_version "231205" \
     --batch_size 10 \
-    --prompt_type 1 \
-    --few_shot
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4" \
+    --dataset_path "../dataset/v3_synthetic/ind/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 1
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4" \
+    --dataset_path "../dataset/v3_synthetic/sun/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 1
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4" \
+    --dataset_path "../dataset/v3_synthetic/ind/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4" \
+    --dataset_path "../dataset/v3_synthetic/sun/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4" \
+    --dataset_path "../dataset/v3_synthetic/ind/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4" \
+    --dataset_path "../dataset/v3_synthetic/sun/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4-1106-preview" \
+    --dataset_path "../dataset/v3_synthetic/ind/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 1
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4-1106-preview" \
+    --dataset_path "../dataset/v3_synthetic/sun/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 1
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4-1106-preview" \
+    --dataset_path "../dataset/v3_synthetic/ind/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4-1106-preview" \
+    --dataset_path "../dataset/v3_synthetic/sun/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4-1106-preview" \
+    --dataset_path "../dataset/v3_synthetic/ind/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "gpt-4-1106-preview" \
+    --dataset_path "../dataset/v3_synthetic/sun/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
