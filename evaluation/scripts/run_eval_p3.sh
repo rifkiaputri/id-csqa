d9f0e66e55bf855fe552b92989a37450fee807a4
Rifki Afina Putri <rifkiafinaputri@gmail.com>
2024-01-19 16:22:38 +0900

    Add merak results & update figures

--- evaluation/scripts/run_eval_p3.sh
+++ evaluation/scripts/run_eval_p3.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=5 python eval.py \
-    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
-    --dataset_path "../dataset/v3_synthetic/filtered_ind.json" \
+CUDA_VISIBLE_DEVICES=2 python eval.py \
+    --model_name "Ichsan2895/Merak-7B-v4" \
+    --dataset_path "../dataset/v1_adapt/ind/filtered_test.json" \
     --gold_key "answer_creator" \
     --history_version "231205" \
     --batch_size 10 \
     --prompt_type 3
 
-CUDA_VISIBLE_DEVICES=5 python eval.py \
-    --model_name "SeaLLMs/SeaLLM-7B-Chat" \
-    --dataset_path "../dataset/v3_synthetic/filtered_sun.json" \
+CUDA_VISIBLE_DEVICES=2 python eval.py \
+    --model_name "Ichsan2895/Merak-7B-v4" \
+    --dataset_path "../dataset/v1_adapt/sun/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=2 python eval.py \
+    --model_name "Ichsan2895/Merak-7B-v4" \
+    --dataset_path "../dataset/v2_human/ind/filtered_test.json" \
+    --gold_key "answer_creator" \
+    --history_version "231205" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=2 python eval.py \
+    --model_name "Ichsan2895/Merak-7B-v4" \
+    --dataset_path "../dataset/v2_human/sun/filtered_test.json" \
     --gold_key "answer_creator" \
     --history_version "231205" \
     --batch_size 10 \
