2b6c12ca849a49cdd37b9ac0b8196d6289213d15
Rifki Afina Putri <rifkiafinaputri@gmail.com>
2024-01-30 14:59:01 +0900

    Add clean benchmark

---
+++ evaluation/scripts/clean/run_eval_polylm_clean.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v1_llm_adapt/ind/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v1_ind" \
+    --batch_size 10 \
+    --prompt_type 1
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v1_llm_adapt/sun/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v1_sun" \
+    --batch_size 10 \
+    --prompt_type 1
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v1_llm_adapt/ind/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v1_ind" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v1_llm_adapt/sun/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v1_sun" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v1_llm_adapt/ind/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v1_ind" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v1_llm_adapt/sun/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v1_sun" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v3_llm_gen/ind/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v3_ind" \
+    --batch_size 10 \
+    --prompt_type 1
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v3_llm_gen/sun/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v3_sun" \
+    --batch_size 10 \
+    --prompt_type 1
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v3_llm_gen/ind/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v3_ind" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v3_llm_gen/sun/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v3_sun" \
+    --batch_size 10 \
+    --prompt_type 2
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v3_llm_gen/ind/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v3_ind" \
+    --batch_size 10 \
+    --prompt_type 3
+
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name "DAMO-NLP-MT/polylm-chat-13b" \
+    --dataset_path "../dataset/v3_llm_gen/sun/filtered_test_clean.json" \
+    --out_name "eval_0-shot_clean.csv" \
+    --gold_key "answer_creator" \
+    --history_version "240124_v3_sun" \
+    --batch_size 10 \
+    --prompt_type 3
